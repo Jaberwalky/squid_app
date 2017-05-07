bf632178915f939cd843deae3ccf2489ded765c5
Paul Jablonski <pauljablonski@me.com>
2017-05-07 13:07:36 +0100

    Add form data to new transaction page.

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -5,8 +5,13 @@ require_relative '../models/category.rb'
 require_relative '../models/account.rb'
 
 get '/transactions/new' do
+  @categories = Category.all()
   erb(:"transactions/new")
 end
+
+# post '/transactions' do
+#
+# end
 
 post '/transactions/:id/delete' do
   Transaction.delete_at_id(params[:id])
